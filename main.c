3ad39335d3259591ab54245fe1a74f48488ae7eb
Asmusrise <asmusrise@gmail.com>
2026-02-17 10:59:35 +0100

    added systick and changes to main

--- main.c
+++ main.c
@@ -8,5 +8,35 @@
  */
 int main(void)
 {
+
+	  INT8U event;
+  INT8U counter_value;
+  INT8U alive_timer = TIM_500_MSEC;
+
+  init_systick();
+  init_gpio();
+
+  // Loop forever.
+  while(1)
+  {
+	// System part of the super loop.
+    // ------------------------------
+	while( !ticks );
+
+	// The following will be executed every 5ms
+    ticks--;
+
+	if( ! --alive_timer )
+	{
+	  alive_timer        = TIM_500_MSEC;
+	  //GPIO_PORTD_DATA_R ^= 0x40; //0100 0000
+	}
+
+	// Application part of the super loop.
+	// -----------------------------------
+	event = select_button();
+	counter_value = counter( event );
+	counter_leds( counter_value );
+  }
 	return 0;
 }
